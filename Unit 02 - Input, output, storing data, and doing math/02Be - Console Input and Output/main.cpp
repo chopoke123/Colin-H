3f6374d0ba207e6bac94330d1fca61e6db43efa6
chopoke123 <78052913+chopoke123@users.noreply.github.com>
2021-01-26 17:51:07 -0600

    Add files via upload

---
+++ Unit 02 - Input, output, storing data, and doing math/02Be - Console Input and Output/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "Cookie Recipe" << endl;
+	cout << "From https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/" << endl;
+	cout << endl;
+	cout << "INGREDIENTS" << endl;
+	cout << "---------------------------" << endl;
+	cout << "1 cup butter, softened" << endl;
+	cout << "1 cup white sugar" << endl;
+	cout << "1 cup packed brown sugar" << endl;
+	cout << "2 eggs" << endl;
+	cout << "2 teaspoons vanilla extract" << endl;
+	cout << "1 teaspoon baking soda" << endl;
+	cout << "2 teaspoons hot water" << endl;
+	cout << "1/2 teaspoon salt" << endl;
+	cout << "3 cups all-purpose flour" << endl;
+	cout << "2 cups semisweet chocolate" << endl;
+	cout << endl;
+	cout << "---------------------------" << endl;
+	cout << endl;
+	cout << "INGREDIENTS" << endl;
+	cout << "1. Preheat oven to 350 degrees F" << endl;
+	cout << endl;
+	cout << "2. Cream together the butter, white sugar, and brown sugar until smooth." << endl;
+	cout << "   Beat in the eggs one at a time, then stir in the vanilla." << endl;
+	cout << "   Dissolve baking soda in hot water. Add to batter along with salt." << endl;
+	cout << "   Stir in flour, chocolate chips, and nuts." << endl;
+	cout << "   Drop by large spoonfuls onto ungreased pans." << endl;
+	cout << endl;
+	cout << "3. Bake for about 10 minutes in the preheated oven." << endl;
+
+		return 0;
+}
