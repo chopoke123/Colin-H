b73e431b1701b4b16a0c4d3e07165b78a4a773b1
chopoke123 <78052913+chopoke123@users.noreply.github.com>
2021-01-26 17:04:51 -0600

    Add files via upload

---
+++ Unit 02 - Input, output, storing data, and doing math/02Ae - Program basics/function.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+void Author()
+{
+    cout << "This program is by Rachel" << endl
+        << "Favorite color: Purple" << endl
+        << "Currently playing: Divinity Original Sin 2" << endl;
+}
